e03bebeed2a14d995c3655100c11bf675c2aba25
shakcmpsc <shakhmardanov@ucsb.edu>
2024-02-11 21:13:38 -0800

    I added get and pull for number 15 issue for the organizations

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationsControllerTests.java
@@ -0,0 +1,147 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.UCSBOrganizations;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = UCSBOrganizationsController.class)
+@Import(TestConfig.class)
+public class UCSBOrganizationsControllerTests extends ControllerTestCase {
+
+        @MockBean
+        UCSBOrganizationsRepository ucsbOrganizationsRepository;
+
+        @MockBean
+        UserRepository userRepository;
+
+
+        @Test
+        public void logged_out_users_cannot_get_all() throws Exception {
+                mockMvc.perform(get("/api/ucsborganizations/all"))
+                                .andExpect(status().is(403)); 
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_users_can_get_all() throws Exception {
+                mockMvc.perform(get("/api/ucsborganizations/all"))
+                                .andExpect(status().is(200)); 
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
+                // arrange
+
+                when(ucsbOrganizationsRepository.findById(eq("KASA"))).thenReturn(Optional.empty());
+
+                MvcResult response = mockMvc.perform(get("/api/ucsborganizations?orgCode=KASA"))
+                                .andExpect(status().isNotFound()).andReturn();
+
+
+                verify(ucsbOrganizationsRepository, times(1)).findById(eq("KASA"));
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("EntityNotFoundException", json.get("type"));
+                assertEquals("UCSBOrganizations with id KASA not found", json.get("message"));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_ucsborganizations() throws Exception {
+
+
+                UCSBOrganizations rowing = UCSBOrganizations.builder()
+                                .orgCode("RC")
+                                .orgTranslationShort("ROWING CLUB")
+                                .orgTranslation("ROWING CLUB AT UCSB")
+                                .inactive(false)
+                                .build();
+
+                UCSBOrganizations chess = UCSBOrganizations.builder()
+                                .orgCode("CC")
+                                .orgTranslationShort("CHESS CLUB")
+                                .orgTranslation("CHESS CLUB AT UCSB")
+                                .inactive(false)
+                                .build();
+
+                ArrayList<UCSBOrganizations> expectedOrganizations = new ArrayList<>();
+                expectedOrganizations.addAll(Arrays.asList(rowing, chess));
+
+                when(ucsbOrganizationsRepository.findAll()).thenReturn(expectedOrganizations);
+
+                MvcResult response = mockMvc.perform(get("/api/ucsborganizations/all"))
+                                .andExpect(status().isOk()).andReturn();
+
+
+                verify(ucsbOrganizationsRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedOrganizations);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+
+        @Test
+        public void logged_out_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/ucsborganizations/post"))
+                                .andExpect(status().is(403));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_regular_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/ucsborganizations/post"))
+                                .andExpect(status().is(403)); // only admins can post
+        }
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void an_admin_user_can_post_a_new_organizations() throws Exception {
+                // arrange
+
+                UCSBOrganizations swimming = UCSBOrganizations.builder()
+                                .orgCode("SC")
+                                .orgTranslationShort("SWIMMING")
+                                .orgTranslation("SWIMMING CLUB AT UCSB")
+                                .inactive(true)
+                                .build();
+
+                when(ucsbOrganizationsRepository.save(eq(swimming))).thenReturn(swimming);
+
+                MvcResult response = mockMvc.perform(
+                                post("/api/ucsborganizations/post?orgCode=SC&orgTranslationShort=SWIMMING&orgTranslation=SWIMMING CLUB AT UCSB&inactive=true")
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
+
+                verify(ucsbOrganizationsRepository, times(1)).save(swimming);
+                String expectedJson = mapper.writeValueAsString(swimming);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+    }
+
