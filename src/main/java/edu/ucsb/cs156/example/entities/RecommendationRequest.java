77c1d2181099005f91c7785b1e1802aaf78be26d
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-12 22:12:14 -0800

    oopsies pls deploy

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -1,27 +1,27 @@
-package edu.ucsb.cs156.example.entities;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.time.LocalDateTime;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Entity(name = "ucsbdates")
-public class RecommendationRequest {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-
-  private String quarterYYYYQ;
-  private String name;  
-  private LocalDateTime localDateTime;
-}
+//package edu.ucsb.cs156.example.entities;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import java.time.LocalDateTime;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@Entity(name = "ucsbdates")
+////public class RecommendationRequest {
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  private long id;
+//
+//  private String quarterYYYYQ;
+//  private String name;
+//  private LocalDateTime localDateTime;
+//}
