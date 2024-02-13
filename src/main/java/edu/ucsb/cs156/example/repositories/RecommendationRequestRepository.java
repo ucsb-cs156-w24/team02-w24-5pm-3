868152d7d3bf23f479f6fdf1de6e41ea7d702651
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-13 00:37:20 -0800

    fixed conflict

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -1,11 +1,11 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RecommendationRequest extends CrudRepository<RecommendationRequest, Long> {
+public interface RecommendationRequestRepository extends CrudRepository<RecommendationRequest, Long> {
 
 }
