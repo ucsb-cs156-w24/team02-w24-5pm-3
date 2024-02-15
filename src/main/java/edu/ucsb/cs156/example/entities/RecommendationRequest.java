f18b569440149453035f4ccc5ce2fcb62381b155
Ben Richardson <97922340+bwr02@users.noreply.github.com>
2024-02-14 21:18:23 -0800

    added first methods (no tests yet)

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -6,17 +6,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "recomendationrequest")
+@Entity(name = "recommendationrequest")
 public class RecommendationRequest {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
